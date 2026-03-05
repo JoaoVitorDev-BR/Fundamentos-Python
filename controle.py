92fcfb79d8c4dcc5f928a1a399214b0fc5359b2c
João Vitor <joao.vitor.dev.br@gmail.com>
2026-03-05 14:29:48 -0300

    Atualizei o código para ficar mais didático

--- controle.py
+++ controle.py
@@ -1,23 +1,28 @@
-#Estruturas de controle
+# Estruturas de Controle em Python
 
 # IF / ELSE
+idade = 20  # definindo a variável idade
 
-idade = 20 
+# Se a idade for maior ou igual a 18, imprime que é maior de idade
 if idade >= 18:
     print("Você é maior de idade")
+# Caso contrário, imprime que é menor de idade
 else:
     print("Você é menor de idade")
 
 
-# FOR 
-
-for i in range (1, 10):
+# FOR
+# O loop for percorre uma sequência de números
+# Aqui, range(1, 10) gera números de 1 até 9
+for i in range(1, 10):
     print("Número:", i)
 
 
 # WHILE
+# O loop while repete enquanto a condição for verdadeira
+cont = 1  # inicializando a variável cont
 
-cont = 1
+# Enquanto cont for menor ou igual a 5, o bloco será executado
 while cont <= 5:
     print("Contagem:", cont)
-cont += 1
+    cont += 1  # incrementa 1 a cada repetição para não ficar infinito
